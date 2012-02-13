0425f84eee2227d02bd26afe48086203e07ed66b
Espen Hovlandsdal <rexxars@gmail.com>
2012-02-13 21:45:26 +0100

    Added some user interaction to the demo page.

---
+++ user-interaction.js
@@ -0,0 +1,39 @@
+(function(d) {
+    var bind = function(el, event, fn) {
+        if (el.addEventListener) {
+            el.addEventListener(event, fn, false);
+        } else {
+            el.attachEvent('on' + event, fn)
+        }
+    };
+
+    var dyn  = d.getElementById('dynamicProgress'), timer, progress = 0;
+    var el   = dyn.getElementsByTagName('progress')[0];
+    var perc = dyn.getElementsByClassName('percentage')[0];
+
+    var progressTo = function(to) {
+        clearInterval(timer);
+        var inc = to < el.value ? -1 : 1;
+
+        timer = setInterval(function() {
+            progress += inc;
+            el.value = progress;
+
+            perc[i].innerHTML = progress;
+
+            if (progress == to) {
+                return clearInterval(timer);
+            }
+        }, 25);
+    };
+
+    bind(d.getElementById('animateFully'), 'click', function() {
+        progressTo(100);
+    });
+
+    bind(d.getElementById('animateToUserInput'), 'click', function() {
+        var to = parseInt(prompt('How many percent?', 50), 10);
+        progressTo(Math.max(0, Math.min(100, to)));
+    });
+
+})(document);
